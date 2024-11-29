5ad21b69d81fff21d65e7312ffd4963d51c3a248
aanasakhtar <anasakhtar851@gmail.com>
2024-11-30 02:54:01 +0500

    some more progress

--- main.py
+++ main.py
@@ -4,115 +4,40 @@ from PyQt6.QtWidgets import QMessageBox, QApplication, QMainWindow, QTableWidget
 import sys
 import pyodbc
 
-# server = "DESKTOP-CHMOJM3\SQLEXPRESS" # Anas
+server = "DESKTOP-CHMOJM3\SQLEXPRESS" # Anas
 # server = 'DESKTOP-9QAGOMJ\SQLSERVER1' # Hamza
-server = 'LAPTOP-N8UU3FAP\SQLSERVER1' #Zoraiz
+# server = 'LAPTOP-N8UU3FAP\SQLSERVER1' #Zoraiz
 database = "DbFinal"
 connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes'
 
 
+def show_message(parent, title, message):
+    """Utility function to show a message box."""
+    msg_box = QtWidgets.QMessageBox(parent)
+    msg_box.setWindowTitle(title)
+    msg_box.setText(message)
+    msg_box.exec()
 
 # Replace these with your own database connection details
+
+############################################ MODULE 1 ################################################### 
+
 class UI(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('Main_screen.ui', self)
-        #self.createAccountBtn = self.findChild(QtWidgets.QPushButton, "CreateAccount_Button")
-        #self.signInCustomerBtn = self.findChild(QtWidgets.QPushButton, "Signin_Customer_Button")
-        #self.signInLibrarianBtn = self.findChild(QtWidgets.QPushButton, "Signin_Librarian_Button")
         
         self.CreateAccount_Button.clicked.connect(self.open_create_account)
-
+        self.Signin_Customer_Button.clicked.connect(self.open_sign_in)
+        self.Signin_Librarian_Button.clicked.connect(self.open_sign_in)
     def open_create_account(self):
-        # Create and display the Create Account screen
-        self.create_account_window =  QtWidgets.QMainWindow()
-        uic.load_ui('Create_account.ui', self.create_account_window )
-        self.close()  # Close the main window (optional)
+       self.creatingAcc = CreateAccount()
+       self.creatingAcc.show()
 
-        #self.inventory_window = QtWidgets.QMainWindow()  # Create a new QMainWindow
-        #uic.loadUi('Inventory.ui', self.inventory_window)  # Load the Inventories UI
-        #self.inventory_window.show()  # Show the Inventories window
-
-    #def createAcc(self):
-     #   self.creatingAcc = CreateAccount()
-      #  self.creatingAcc.show()
+    def open_sign_in(self):
+        pass
 
 
-        # self.books_tw.itemSelectionChanged.connect(self.enable_buttons)
-
-        # self.booksTableWidget.setRowCount(len(books))
-        
-        # for i in range(len(books)):
-        #     for j in range(5):
-        #         item = QtWidgets.QTableWidgetItem(books[i][j])
-        #         # Make the items non-editable
-        #         item.setFlags(Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsSelectable) 
-        #         self.booksTableWidget.setItem(i,j,item)
-
-        # Connect the search function with the search button.
-        # self.search_pb.clicked.connect(self.search)
-
-        # Connect the view function with the view button.
-        # self.view_pb.clicked.connect(self.view)
-
-        # Connect the delete function with the delete button.
-        # self.delete_pb.clicked.connect(self.delete)
-
-        # Connect the close function with the close button.
-        # self.close_pb.clicked.connect(self.close)
-    # def booktype(self):
-        # for selected_type in self.booktype_rb.buttons():
-    #         if selected_type.isChecked():
-    #             type_text = selected_type.text()
-    #             return type_text
-    #     return ""
-
-
-    # def search(self):
-    #     # TO BE IMPLEMENTED
-    #     selected_category = self.category_cb.currentText()
-    #     title_text = self.title_le.text().lower()
-    #     for row in range(self.books_tw.rowCount()):
-    #         category = self.books_tw.item(row, 2)
-    #         title = self.books_tw.item(row, 1)
-    #         book_type = self.books_tw.item(row, 3)
-    #         issuance = self.books_tw.item(row, 4)
-    #         category_matches = (selected_category == "" or selected_category == category.text())
-    #         title_matches = (title_text in title.text().lower() or title_text == "")
-    #         type_matches = (book_type.text() == self.booktype() or self.booktype() == "")
-    #         if self.issue_cb.isChecked():
-    #             if issuance.text() == "True":
-    #                 self.books_tw.setRowHidden(row, not (category_matches and title_matches and type_matches))
-    #             else:
-    #                 self.books_tw.setRowHidden(row, not (category_matches and title_matches and type_matches and False))
-                
-    #         else:
-    #             self.books_tw.setRowHidden(row, not (category_matches and title_matches and type_matches))
-    
-    # def enable_buttons(self):
-    #     if self.books_tw.selectedItems():
-    #         self.view_pb.setEnabled(True)
-    #         self.delete_pb.setEnabled(True)
-    #     else:
-    #         self.view_pb.setEnabled(False)
-    #         self.delete_pb.setEnabled(False)
-    
-    
-    # def delete(self): 
-    #     selected_row = self.books_tw.currentRow()
- #     if selected_row != -1:
-    #         conf = QMessageBox()
-    #         conf.setIcon(QMessageBox.Icon.Warning)
-    #         conf.setText("Are you sure you want to delete this book?")
-    #         conf.setWindowTitle("Confirmation Box")
-    #         conf.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
-    #         respnse = conf.exec()
-    #         if respnse == QMessageBox.StandardButton.Yes:
-    #             self.books_tw.removeRow(selected_row)
-
-
-    # def close(self):
-    #     QtWidgets.QApplication.quit()
             
 class CreateAccount(QtWidgets.QMainWindow):  
     def _init_(self):
@@ -133,32 +58,117 @@ class CreateAccount(QtWidgets.QMainWindow):
             c_password = self.cPasswordF.text()
 
             if not username or not password or not c_password:
-                self.show_message("Error", "All fields must be filled.")
+                show_message(self, "Error", "All fields must be filled.")
                 return
 
             if password != c_password:
-                self.show_message("Error", "Passwords do not match.")
+                show_message(self,"Error", "Passwords do not match.")
                 return
 
+            for i in username:
+                if not i.isalpha():
+                    show_message(self, "Error", "Username only contains alphabets.")
+                    return
+            check_query = """
+                            SELECT COUNT(*)
+                            FROM Member_Info
+                            WHERE Member_Name = ?
+                          """
+            connection = pyodbc.connect(connection_string)
+
+            cursor = connection.cursor()
+
+            cursor = connection.cursor()
+            cursor.execute(check_query, (username,))
+            result = cursor.fetchone()
+            if result[0] > 0:
+                show_message(self, "Error", "Username already exists.")
             try:
                 # Insert data into the database
                 self.add_to_database(username, password)
-                self.show_message("Success", "Account created successfully!")
+                show_message(self, "Success", "Account created successfully!")
             except Exception as e:
-                self.show_message("Error", f"Failed to create account: {e}")
+                show_message(self, "Error", f"Failed to create account: {e}")
 
     def add_to_database(self, username, password): # Not made yet
         # Database connection
-        pass
+        connection = pyodbc.connect(connection_string)
 
-    def show_message(self, title, message):
-        """Utility function to show a message box."""
-        pass
-        msg_box = QtWidgets.QMessageBox(self)
-        msg_box.setWindowTitle(title)
-        msg_box.setText(message)
-        msg_box.exec()
+        cursor = connection.cursor()
 
+        cursor = connection.cursor()
+
+        sql_query = """
+            insert into Member_Info(Member_Name, Member_Password, Member_Status)
+            values (?,?,?)
+            """
+
+        cursor.execute(sql_query, (username, password, "Active"))
+        connection.commit()
+
+    
+
+
+class SignIn(QtWidgets.QMainWindow):
+    def __init__(self):
+        super()._init_() 
+        # Load the .ui file
+        uic.loadUi('Sign_in.ui', self)
+        username = self.findChild(QtWidgets.QTextEdit, "Enter_Username_Edit")
+        password = self.findChild(QtWidgets.QTextEdit, "Enter_Password_Edit")
+        if "." in username:
+            self.signInBtn.clicked.connect(self.signInAsLibrarian(username, password))
+        else:
+            self.signInBtn.clicked.connect(self.signInAsMember(username, password))
+    def signInAsLibrarian(self, username, password):
+        # Database connection
+        connection = pyodbc.connect(connection_string)
+
+        cursor = connection.cursor()
+
+        cursor = connection.cursor()
+
+        sql_query = """
+            select Librarian_Password from Library_Staff where Librarian_Name = ?
+            """
+
+        cursor.execute(sql_query, (username))
+        result = cursor.fetchone()
+        if password != result[0]:
+            show_message(self, "Error", "Incorrect Password")
+            return
+        if result is None:
+            show_message(self, "Error", "No username found")
+            return
+        self.openLibrarianScreen()
+    def openLibrarianScreen(self):
+       self.librarianScreen = Admin_or_Librarian()
+       self.librarianScreen.show()
+    
+    def signInAsMember(self, username, password):
+        # Database connection
+        connection = pyodbc.connect(connection_string)
+
+        cursor = connection.cursor()
+
+        cursor = connection.cursor()
+
+        sql_query = """
+            select Member_Password from Member_Info where Member_Name = ?
+            """
+
+        cursor.execute(sql_query, (username))
+        result = cursor.fetchone()
+        if password != result[0]:
+            show_message(self, "Error", "Incorrect Password")
+            return
+        if result is None:
+            show_message(self, "Error", "No username found")
+            return
+        self.openMemberScreen()
+    def openMemberScreen(self): 
+       self.memberScreen = MemberScreen()
+       self.memberScreen.show()
 
 
 ############################################ MODULE 2 ################################################### 
@@ -354,63 +364,78 @@ class Members(QtWidgets.QMainWindow):
                 # Update the status in UI purposes
                 self.Members_Table.item(selected_row, 3).setText("Inactive")
 
-#CUSTOMER SCREENN!!!!!
-    # Connect buttons
-    #     self.SearchIssue_Button.clicked.connect(self.open_search_issue)
-    #     self.BookRoom_Button.clicked.connect(self.open_book_room)
 
-    # def open_search_issue(self):
-    #     """Open the Search Issue window."""
-    #     self.search_issue_window = QtWidgets.QMainWindow()  # Create a new QMainWindow
-    #     uic.loadUi('SearchIssue.ui', self.search_issue_window)  # Load the Search Issue UI
-    #     self.search_issue_window.show()  # Show the Search Issue window
+############################################ MODULE 3 ################################################### 
 
-    # def open_book_room(self):
-    #     """Open the Book Room window."""
-    #     self.book_room_window = QtWidgets.QMainWindow()  # Create a new QMainWindow
-    #     uic.loadUi('BookRoom.ui', self.book_room_window)  # Load the Book Room UI
-    #     self.book_room_window.show()  # Show the Book Room window
+class MemberScreen(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Member_or_customer.ui', self)
+        self.SearchIssue_Button.clicked.connect(self.openSearchScreen)
+        self.BookRoom_Button.clicked.connect(self.openBookARoomScreen)
+    def openSearchScreen(self):
+        self.searchScreen = SearchScreen()
+        self.searchScreen.show()
+    def openBookARoomScreen():
+        pass
 
+class SearchScreen:
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("Search.ui")
+        self.GenreCB.setCurrentText(-1)
+        self.TitleF = self.findChild(QtWidgets.QLineEdit, "TitleLE")
+        self.GenreF = self.findChild(QtWidgets.QComboBox, "GenreCB")
+        self.AuthorF = self.findChild(QtWidgets.QLineEdit, "AuthorLE")
+        connection = pyodbc.connect(connection_string)
+
+        cursor = connection.cursor()
+
+        cursor = connection.cursor()
+
+        cursor.execute("select * from Books")
+
+        # Fetch all rows and populate the table
+        for row_index, row_data in enumerate(cursor.fetchall()):
+            self.BookTW.insertRow(row_index)
+            for col_index, cell_data in enumerate(row_data):
+                item = QTableWidgetItem(str(cell_data))
+                self.bookTW.setItem(row_index, col_index, item)
+
+        self.SearchPB.clicked.connect(self.search)
+        self.ViewPB.clicked.connect(self.view) # Yet to be completed
+        self.RateABookPB.clicked.connect(self.rateABook) # Yet to be completed
+
+        self.ViewAllPB.clicked.connect(self.viewAll)
+        self.IssuePB.clicked.connect(self.issue) # Yet to be completed
+
+    def search(self):
+        sqlQuery = "select * from Books where "
+        parameters = []
+        if self.TitleF.text():
+            sqlQuery+="and Title=? "
+            parameters.append(self.TitleF.text())
+        if self.GenreF.text():
+            sqlQuery+="and Genre=? "
+            parameters.append(self.GenreF.text())
+        if self.AuthorF.text():
+            sqlQuery+="and Author=? "
+            parameters.append(self.AuthorF.text())
+
+        self.cursor.execute(sqlQuery, parameters)
     
+    def viewAll(self):
+        self.cursor.execute("select * from Books")
 
-# class ViewBook(QtWidgets.QMainWindow):  
-#     def _init_(self, isbn,title,category,type,issuance):
-#         super()._init_() 
-#         # Load the .ui file
-#         uic.loadUi('ViewBook.ui', self)
+        # Fetch all rows and populate the table
+        for row_index, row_data in enumerate(self.cursor.fetchall()):
+            self.BookTW.insertRow(row_index)
+            for col_index, cell_data in enumerate(row_data):
+                item = QTableWidgetItem(str(cell_data))
+                self.bookTW.setItem(row_index, col_index, item)
 
-#         self.findChild(QtWidgets.QLineEdit, 'ISBNLineEdit').setText(isbn)
-#         self.findChild(QtWidgets.QLineEdit, 'TitleLineEdit').setText(title)
-#         self.findChild(QtWidgets.QLineEdit, 'CategoryLineEdit').setText(category)
 
-#         if type == "Reference Book": 
-#             self.findChild(QtWidgets.QRadioButton, "RefBookRadioButton").setChecked(True)
-#             self.findChild(QtWidgets.QRadioButton, "TextBookRadioButton").setChecked(False)
-#             self.findChild(QtWidgets.QRadioButton, "JournalRadioButton").setChecked(False)
-#         elif type == "Text Book": 
-#             self.findChild(QtWidgets.QRadioButton, "RefBookRadioButton").setChecked(False)
-#             self.findChild(QtWidgets.QRadioButton, "TextBookRadioButton").setChecked(True)
-#             self.findChild(QtWidgets.QRadioButton, "JournalRadioButton").setChecked(False)
-#         else: 
-#             self.findChild(QtWidgets.QRadioButton, "RefBookRadioButton").setChecked(False)
-#             self.findChild(QtWidgets.QRadioButton, "TextBookRadioButton").setChecked(False)
-#             self.findChild(QtWidgets.QRadioButton, "JournalRadioButton").setChecked(True)
 
-#         self.findChild(QtWidgets.QRadioButton, "RefBookRadioButton").setEnabled(False)
-#         self.findChild(QtWidgets.QRadioButton, "TextBookRadioButton").setEnabled(False)
-#         self.findChild(QtWidgets.QRadioButton, "JournalRadioButton").setEnabled(False)
-            
-#         if issuance:
-#             self.findChild(QtWidgets.QCheckBox, "IssuedCheckBox").setChecked(True)
-#         self.findChild(QtWidgets.QCheckBox, "IssuedCheckBox").setEnabled(False)
-            #         self.make_form_read_only()
-
-#     def make_form_read_only(self):
-#         for widget in self.findChildren(QtWidgets.QWidget):
-#             if isinstance(widget, (QtWidgets.QLineEdit, QtWidgets.QTextEdit)):
-#                 widget.setReadOnly(True)
-#             elif isinstance(widget, (QtWidgets.QComboBox, QtWidgets.QPushButton)):
-#                 widget.setEnabled(False)
 
 
 
@@ -418,5 +443,3 @@ app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplic
 window = UI() # Create an instance of our 
 window.show()
 app.exec() # Start the application
-
-##
